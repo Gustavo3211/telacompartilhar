6bb944fff13d4483ee2343b7df8039971cf2ce68
Developer <developer@example.com>
2026-08-18 19:17:19 -0300

    Configure base path in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,5 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Set base path for GitHub Pages deployment.
-  // Relative path './' works for any repo name, or you can specify '/repo-name/'
-  base: './',
+  base: '/telacompartilhar/',
 });
